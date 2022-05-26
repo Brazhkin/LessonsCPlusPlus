8e4e269b0b04677f13b72f16052fdbc87a4eb0d3
Brazhkin Alexey <aa.brazhkin@gamail.com>
2022-05-26 18:36:17 +0300

    345

--- main.cpp
+++ main.cpp
@@ -3,28 +3,21 @@
 
 int main() {
 
-<<<<<<< HEAD
-    cout <<3333333;
-
-    std::vector<std::vector<int>> v(10, std::vector<int>(10));
-=======
     using namespace std;
 
     cout << "111111";
 
     vector<int> v;
 
-    void print_vector;
-    print_vector = [&]() {
+    auto print_vector = [&]() {
         for (char c: v)
             std::cout << c << ' ';
         std::cout << '\n';
     };
->>>>>>> BRAZHKIN
 
     for (auto & i : v)
-        for (auto j = 0; j < i.size(); j++)
-            i[j] = j;
+        for (auto j = 0; j < v.size(); j++)
+            i = j;
 
     return 0;
 }
