4d2249ccc1243ea4e07ea1dd141aa9c510a7e27f
Brazhkin Alexey <abrazhkin@s-terra.com>
2022-05-30 18:56:13 +0300

    c++/constructor

--- main.cpp
+++ main.cpp
@@ -1,28 +1,60 @@
-#include <vector>
 #include <iostream>
+#include <vector>
+#include <string>
 #include <algorithm>
-
 using namespace std;
 
-struct print
-{
-    void operator()(int element)
+class Box {
+public:
+    Box() { std::cout << "default" << std::endl; }
+    Box(int width, int height, int length)
+            : m_width(width), m_height(height), m_length(length)
     {
-        cout << element << endl;
+        std::cout << "int,int,int" << std::endl;
     }
+    Box(Box& other)
+            : m_width(other.m_width), m_height(other.m_height), m_length(other.m_length)
+    {
+        std::cout << "copy" << std::endl;
+    }
+    [[nodiscard]] Box(Box&& other) noexcept : m_width(other.m_width), m_height(other.m_height), m_length(other.m_length)
+    {
+        m_contents = std::move(other.m_contents);
+        std::cout << "move" << std::endl;
+    }
+    [[nodiscard]] int Volume() const { return m_width * m_height * m_length; }
+    void Add_Item(const string& item) { m_contents.push_back(item); }
+    void Print_Contents()
+    {
+        for (const auto& item : m_contents)
+        {
+            cout << item << " ";
+        }
+    }
+private:
+    int m_width{ 0 };
+    int m_height{ 0 };
+    int m_length{ 0 };
+    vector<string> m_contents;
 };
 
-cout << "4444444444444444444";
-
-cout << "12930219389213";
-
-cout << "888";
-
-int main(void)
+Box get_Box()
 {
-    std::vector<int> v = {1, 2, 3, 4, 5};
+    Box b(5, 10, 18); // "int,int,int"
+    b.Add_Item("Toupee");
+    b.Add_Item("Megaphone");
+    b.Add_Item("Suit");
 
-    std::for_each(v.begin(), v.end(), print());
+    return b;
+}
+
+int main()
+{
+    Box b; // "default"
+    Box b1(b); // "copy"
+    Box b2(get_Box()); // "move"
+    cout << "b2 contents: ";
+    b2.Print_Contents(); // Prove that we have all the values
 
     return 0;
 }
